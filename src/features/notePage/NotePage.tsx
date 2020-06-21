251e805c78d1a9b889c2a21cfd812028fb5d7972
Jake Weathington <kennethjweathington@gmail.com>
2020-06-20 21:38:43 -0400

    Updated Chessground to fix resize bug

--- src/features/notePage/NotePage.tsx
+++ src/features/notePage/NotePage.tsx
@@ -1,24 +1,22 @@
 import './styles/chessground.css';
 import React, { useState } from 'react';
 import Editorjs from 'react-editor-js';
-import EditorJS, { OutputData } from '@editorjs/editorjs';
+import EditorJS from '@editorjs/editorjs';
 import ChessBoardTool from './ChessBoardTool';
 const Paragraph = require('@editorjs/paragraph');
 const List = require('@editorjs/list');
 
 interface NotePageProps {}
 
-export const NotePage: React.FC<NotePageProps> = ({}) => {
-  // const data =
-  //   '{"time":1592493985004,"blocks":[{"type":"paragraph","data":{"text":"sdfsdf"}},{"type":"paragraph","data":{"text":"gggsdf"}},{"type":"board","data":{"fen":"r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R"}}],"version":"2.18.0"}';
+export const NotePage: React.FC<NotePageProps> = () => {
   const [editor, setEditor] = useState<EditorJS>();
 
   async function save() {
     try {
       if (editor) {
         const saveData = await editor.save();
+
         sessionStorage.setItem('testSaveData', JSON.stringify(saveData));
-        // console.log(JSON.stringify(saveData));
       } else throw new Error('Editor is null!');
     } catch (error) {
       console.log(error);
